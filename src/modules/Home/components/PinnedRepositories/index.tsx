5797edff983d99186a5e81c8a102a1330b3be619
Rogix <12768403+rogix@users.noreply.github.com>
2022-08-07 23:06:34 -0300

    feat: pinned repositories list

---
+++ src/modules/Home/components/PinnedRepositories/index.tsx
@@ -0,0 +1,62 @@
+import { useQuery, gql } from '@apollo/client'
+import { Box, Item } from './style'
+import { RiGitRepositoryLine } from 'react-icons/ri'
+
+const GET_PINNED_REPOSITORIES = gql`
+  query PinnedRepositories {
+    viewer {
+      pinnedItems(last: 6) {
+        edges {
+          node {
+            ... on Repository {
+              id
+              name
+              description
+              url
+              languages(last: 10) {
+                edges {
+                  node {
+                    id
+                    name
+                    color
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export function PinnedRepositories() {
+  const { loading, error, data } = useQuery(GET_PINNED_REPOSITORIES)
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
+  if (error) {
+    return <p>Error :(</p>
+  }
+
+  const { viewer } = data
+
+  return (
+    <Box>
+      <h2>Pinned Repositories</h2>
+      <Item>
+        {viewer.pinnedItems.edges.map(({ node }: any) => (
+          <li key={node.id}>
+            <div>
+              <RiGitRepositoryLine />
+              <a href={node.url}>{node.name}</a>
+            </div>
+            Created At: {node.createdAt}
+          </li>
+        ))}
+      </Item>
+    </Box>
+  )
+}
