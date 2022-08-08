534273fe9353cadcdc7f7ca900bb1b408bd5e00a
Rogix <12768403+rogix@users.noreply.github.com>
2022-08-07 23:04:17 -0300

    feat: header styles and avatar

--- src/modules/Home/components/Header/index.tsx
+++ src/modules/Home/components/Header/index.tsx
@@ -1,10 +1,32 @@
 import { Search } from '@/components/Search'
 import { HeaderBox } from './style'
+import Image from 'next/future/image'
 
-export function Header() {
+type Props = {
+  name: string
+  bio: string
+  avatar: string
+}
+
+const imgStyle = { borderRadius: '50%', marginRight: '1rem' }
+
+export function Header({ avatar, name, bio }: Props) {
   return (
     <HeaderBox>
-      <h2>Main</h2>
+      <div>
+        <Image
+          src={avatar}
+          alt={name}
+          width={40}
+          height={40}
+          className="avatar"
+          style={imgStyle}
+        />
+        <div>
+          <h2>{name}</h2>
+          <p>{bio}</p>
+        </div>
+      </div>
       <Search />
     </HeaderBox>
   )
--- src/modules/Home/components/Header/style.ts
+++ src/modules/Home/components/Header/style.ts
@@ -6,7 +6,13 @@ export const HeaderBox = styled.header`
   justify-content: space-between;
   width: 100%;
   padding-bottom: 20px;
+  margin: 20px 0;
   color: #fff;
   height: 50px;
   border-radius: 8px;
+
+  > div {
+    display: flex;
+    align-items: center;
+  }
 `
