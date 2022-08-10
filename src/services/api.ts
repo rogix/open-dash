84b3c88f3ef89124bec8850c3143eb6e115451cb
Rogix <12768403+rogix@users.noreply.github.com>
2022-08-10 09:43:16 -0300

    feat: add chromiun aws lambda

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,33 +1,47 @@
 import puppeteer from 'puppeteer'
+import chromium from 'chrome-aws-lambda'
 
 const url = 'https://github.com/trending'
 
 const getTrendingRepositories = async () => {
-  const browser = await puppeteer.launch({ headless: true })
-  const page = await browser.newPage()
-  await page.goto(url)
-  const data = await page
-    ?.evaluate(() => {
-      const repositories = Array.from(document.querySelectorAll('article'))
-      return repositories?.map(repo => {
-        const [name, description, stars, forks, starsToday] = Array.from(
-          repo.querySelectorAll(
-            'h1 a, p, div:nth-of-type(2) > a, div:nth-of-type(2) > a:nth-of-type(2), div:nth-of-type(2) > span.float-sm-right',
-          ),
-        )
+  let browser = null
+  let page = null
 
-        return {
-          name: name?.innerText || '',
-          description: description?.innerText || '',
-          forks: forks?.innerText,
-          stars: stars?.innerText || '',
-          starsToday: starsToday?.innerText || '',
-        }
-      })
+  try {
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: true,
     })
-    .catch(error => error)
-  await browser.close()
-  return data
+
+    page = await browser.newPage()
+    await page.goto(url)
+    const data = await page
+      ?.evaluate(() => {
+        const repositories = Array.from(document.querySelectorAll('article'))
+        return repositories?.map(repo => {
+          const [name, description, stars, forks, starsToday] = Array.from(
+            repo.querySelectorAll(
+              'h1 a, p, div:nth-of-type(2) > a, div:nth-of-type(2) > a:nth-of-type(2), div:nth-of-type(2) > span.float-sm-right',
+            ),
+          )
+
+          return {
+            name: name?.innerText || '',
+            description: description?.innerText || '',
+            forks: forks?.innerText,
+            stars: stars?.innerText || '',
+            starsToday: starsToday?.innerText || '',
+          }
+        })
+      })
+      .catch(error => error)
+    return data
+  } finally {
+    if (browser) {
+      await browser.close()
+    }
+  }
 }
 
 export { getTrendingRepositories }
